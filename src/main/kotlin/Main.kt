5b7ef4bc7f2ecbb2db4a12054a08fe97648315c8
Vallary Buraje <vallaryburaje@gmail.com>
2023-02-02 12:59:43 +0300

    variables added

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+    var fullname = "vallary"
+    var age = 20
+    var phonenumber ="0758797430"
+    var weightInKg =65
+    var isKenyan: Boolean = true
+    var notKenyan: Boolean = false
+
+    println(isKenyan)
+}
+
+
+
+
+
